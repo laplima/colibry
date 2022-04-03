8459c5ef4ca869ab2cd1e4c2d305f91022fad92b
Luiz Lima Jr <llimajr@gmail.com>
2022-04-03 16:37:41 -0300

    Update ORBManager to use auto for T_var variables
    TODO: check if copying _var will duplicate or transfer wonership.

--- sources/orbutils/ORBManager.cpp
+++ sources/orbutils/ORBManager.cpp
@@ -11,14 +11,14 @@ ORBManager::ORBManager(const std::string& orbname)
 	init(orbname);
 }
 
-ORBManager::ORBManager(int argc, char* argv[], const string& orbname)
+ORBManager::ORBManager(int argc, char** argv, const string& orbname)
 {
 	init(argc,argv,orbname);
 }
 
 ORBManager::ORBManager(CORBA::ORB_ptr orb) : orbname_{orb->id()}
 {
-	m_ORB = CORBA::ORB::_duplicate(orb);
+	orb_ = CORBA::ORB::_duplicate(orb);
 }
 
 ORBManager::ORBManager(const ORBManager& om)
@@ -30,9 +30,9 @@ ORBManager::~ORBManager()
 {
 	// std::cout << "ORBM " << orbname_ << " to be destroyed\n";
 	if (initiated()) {
-		if (!CORBA::is_nil(m_rootpoa.in()))
-			m_rootpoa->destroy(true,true);
-		m_ORB->destroy();
+		if (!CORBA::is_nil(rootpoa_.in()))
+			rootpoa_->destroy(true,true);
+		orb_->destroy();
 	}
 	// std::cout << "ORBM " << orbname_ << " destroyed\n";
 }
@@ -41,15 +41,15 @@ ORBManager& ORBManager::operator=(const ORBManager& om)
 {
 	if (&om != this) {
 		orbname_ = om.orbname_;
-		m_ORB = CORBA::ORB::_nil();
-		m_rootpoa = PortableServer::POA::_nil();
-		m_poamgr = PortableServer::POAManager::_nil();
-		if (!CORBA::is_nil(om.m_ORB.in()))
-			m_ORB = CORBA::ORB::_duplicate(om.m_ORB.in());
-		if (!CORBA::is_nil(om.m_rootpoa.in()))
-			m_rootpoa = PortableServer::POA::_duplicate(om.m_rootpoa.in());
-		if (!CORBA::is_nil(om.m_poamgr.in()))
-			m_poamgr = PortableServer::POAManager::_duplicate(om.m_poamgr.in());
+		orb_ = CORBA::ORB::_nil();
+		rootpoa_ = PortableServer::POA::_nil();
+		poamgr_ = PortableServer::POAManager::_nil();
+		if (!CORBA::is_nil(om.orb_.in()))
+			orb_ = CORBA::ORB::_duplicate(om.orb_.in());
+		if (!CORBA::is_nil(om.rootpoa_.in()))
+			rootpoa_ = PortableServer::POA::_duplicate(om.rootpoa_.in());
+		if (!CORBA::is_nil(om.poamgr_.in()))
+			poamgr_ = PortableServer::POAManager::_duplicate(om.poamgr_.in());
 	}
 	return *this;
 }
@@ -58,17 +58,17 @@ ORBManager& ORBManager::operator=(CORBA::ORB_ptr orb)
 {
 	orbname_ = orb->id();
 
-	m_ORB = (!CORBA::is_nil(orb)) ? CORBA::ORB::_duplicate(orb) : CORBA::ORB::_nil();
-	m_rootpoa = PortableServer::POA::_nil();
-	m_poamgr = PortableServer::POAManager::_nil();
+	orb_ = (!CORBA::is_nil(orb)) ? CORBA::ORB::_duplicate(orb) : CORBA::ORB::_nil();
+	rootpoa_ = PortableServer::POA::_nil();
+	poamgr_ = PortableServer::POAManager::_nil();
 	return *this;
 }
 
-void ORBManager::init(int argc, char* argv[], const string& orbname)
+void ORBManager::init(int argc, char** argv, const string& orbname)
 {
 	// always initiate (may return existing ORB if orbname has been
 	// previously initiated and not destroyed)
-	m_ORB = CORBA::ORB_init(argc,argv,orbname.c_str());
+	orb_ = CORBA::ORB_init(argc,argv,orbname.c_str());
 	orbname_ = orbname;
 }
 
@@ -81,7 +81,7 @@ void ORBManager::init(const std::string& orbname)
 
 bool ORBManager::initiated() const
 {
-	return (!CORBA::is_nil(m_ORB.in()));
+	return (!CORBA::is_nil(orb_.in()));
 }
 
 void ORBManager::check_init()
@@ -93,70 +93,71 @@ void ORBManager::check_init()
 void ORBManager::activate_rootpoa()
 {
 	check_init();
-	m_rootpoa = bootstrap<PortableServer::POA>("RootPOA");
-	m_poamgr = m_rootpoa->the_POAManager();
-	m_poamgr->activate();
+	rootpoa_ = bootstrap<PortableServer::POA>("RootPOA");
+	poamgr_ = rootpoa_->the_POAManager();
+	poamgr_->activate();
 }
 
-PortableServer::POA_var ORBManager::create_child_poa(const std::string& name, const std::vector<Policy>& policies)
+PortableServer::POA_var ORBManager::create_child_poa(const std::string& name, const std::vector<POAPolicy>& policies)
 {
 	check_init();
 	CORBA::PolicyList cpolicies(policies.size());
 	cpolicies.length(policies.size());
 
 	unsigned int count = 0;
-	for (Policy p : policies) {
+	for (POAPolicy p : policies) {
 		switch (p) {
-		case Policy::SYSTEM_ID:
-			cpolicies[count] = m_rootpoa->create_id_assignment_policy(PortableServer::SYSTEM_ID);
+			using enum POAPolicy;
+		case SYSTEM_ID:
+			cpolicies[count] = rootpoa_->create_id_assignment_policy(PortableServer::SYSTEM_ID);
 			break;
-		case Policy::USER_ID:
-			cpolicies[count] = m_rootpoa->create_id_assignment_policy(PortableServer::USER_ID);
+		case USER_ID:
+			cpolicies[count] = rootpoa_->create_id_assignment_policy(PortableServer::USER_ID);
 			break;
-		case Policy::IMPLICIT_ACTIVATION: // default
-			cpolicies[count] = m_rootpoa->create_implicit_activation_policy(PortableServer::IMPLICIT_ACTIVATION);
+		case IMPLICIT_ACTIVATION: // default
+			cpolicies[count] = rootpoa_->create_implicit_activation_policy(PortableServer::IMPLICIT_ACTIVATION);
 			break;
-		case Policy::NO_IMPLICIT_ACTIVATION:
-			cpolicies[count] = m_rootpoa->create_implicit_activation_policy(PortableServer::NO_IMPLICIT_ACTIVATION);
+		case NO_IMPLICIT_ACTIVATION:
+			cpolicies[count] = rootpoa_->create_implicit_activation_policy(PortableServer::NO_IMPLICIT_ACTIVATION);
 			break;
-		case Policy::UNIQUE_ID: // default
-			cpolicies[count] = m_rootpoa->create_id_uniqueness_policy(PortableServer::UNIQUE_ID);
+		case UNIQUE_ID: // default
+			cpolicies[count] = rootpoa_->create_id_uniqueness_policy(PortableServer::UNIQUE_ID);
 			break;
-		case Policy::MULTIPLE_ID:
-			cpolicies[count] = m_rootpoa->create_id_uniqueness_policy(PortableServer::MULTIPLE_ID);
+		case MULTIPLE_ID:
+			cpolicies[count] = rootpoa_->create_id_uniqueness_policy(PortableServer::MULTIPLE_ID);
 			break;
-		case Policy::TRANSIENT: // default
-			cpolicies[count] = m_rootpoa->create_lifespan_policy(PortableServer::TRANSIENT);
+		case TRANSIENT: // default
+			cpolicies[count] = rootpoa_->create_lifespan_policy(PortableServer::TRANSIENT);
 			break;
-		case Policy::PERSISTENT:
-			cpolicies[count] = m_rootpoa->create_lifespan_policy(PortableServer::PERSISTENT);
+		case PERSISTENT:
+			cpolicies[count] = rootpoa_->create_lifespan_policy(PortableServer::PERSISTENT);
 			break;
-		case Policy::USE_ACTIVE_OBJECT_MAP_ONLY:	// default
-			cpolicies[count] = m_rootpoa->create_request_processing_policy(PortableServer::USE_ACTIVE_OBJECT_MAP_ONLY);
+		case USE_ACTIVE_OBJECT_MAP_ONLY:	// default
+			cpolicies[count] = rootpoa_->create_request_processing_policy(PortableServer::USE_ACTIVE_OBJECT_MAP_ONLY);
 			break;
-    	case Policy::USE_DEFAULT_SERVANT:
-			cpolicies[count] = m_rootpoa->create_request_processing_policy(PortableServer::USE_DEFAULT_SERVANT);
+    	case USE_DEFAULT_SERVANT:
+			cpolicies[count] = rootpoa_->create_request_processing_policy(PortableServer::USE_DEFAULT_SERVANT);
     		break;
-    	case Policy::USE_SERVANT_MANAGER:
-			cpolicies[count] = m_rootpoa->create_request_processing_policy(PortableServer::USE_SERVANT_MANAGER);
+    	case USE_SERVANT_MANAGER:
+			cpolicies[count] = rootpoa_->create_request_processing_policy(PortableServer::USE_SERVANT_MANAGER);
     		break;
-    	case Policy::RETAIN:	// default
-			cpolicies[count] = m_rootpoa->create_servant_retention_policy(PortableServer::RETAIN);
+    	case RETAIN:	// default
+			cpolicies[count] = rootpoa_->create_servant_retention_policy(PortableServer::RETAIN);
     		break;
-    	case Policy::NON_RETAIN:
-			cpolicies[count] = m_rootpoa->create_servant_retention_policy(PortableServer::NON_RETAIN);
+    	case NON_RETAIN:
+			cpolicies[count] = rootpoa_->create_servant_retention_policy(PortableServer::NON_RETAIN);
     		break;
-    	case Policy::ORB_CTRL_MODEL:	// default
-			cpolicies[count] = m_rootpoa->create_thread_policy(PortableServer::ORB_CTRL_MODEL);
+    	case ORB_CTRL_MODEL:	// default
+			cpolicies[count] = rootpoa_->create_thread_policy(PortableServer::ORB_CTRL_MODEL);
     		break;
-    	case Policy::SINGLE_THREAD_MODEL:
-			cpolicies[count] = m_rootpoa->create_thread_policy(PortableServer::SINGLE_THREAD_MODEL);
+    	case SINGLE_THREAD_MODEL:
+			cpolicies[count] = rootpoa_->create_thread_policy(PortableServer::SINGLE_THREAD_MODEL);
 			break;
 		}
 		++count;
 	}
 
-	PortableServer::POA_ptr childpoa = m_rootpoa->create_POA(name.c_str(),m_poamgr,cpolicies);
+	PortableServer::POA_ptr childpoa = rootpoa_->create_POA(name.c_str(),poamgr_,cpolicies);
 
 	for (count=0; count<cpolicies.length(); ++count)
 		cpolicies[count]->destroy();
@@ -167,15 +168,15 @@ PortableServer::POA_var ORBManager::create_child_poa(const std::string& name, co
 string ORBManager::object_to_string(CORBA::Object_ptr obj)
 {
 	check_init();
-	CORBA::String_var s = m_ORB->object_to_string(obj);
+	CORBA::String_var s = orb_->object_to_string(obj);
 	return string{s.in()};
 }
 
-CORBA::Object_ptr ORBManager::string_to_object(const string& ior)
-{
-	check_init();
-	return m_ORB->string_to_object(ior.c_str());
-}
+// CORBA::Object_ptr ORBManager::string_to_object(const string& ior)
+// {
+// 	check_init();
+// 	return orb_->string_to_object(ior.c_str());
+// }
 
 void ORBManager::save_ior(const std::string& fname, CORBA::Object_ptr obj)
 {
@@ -188,6 +189,6 @@ void ORBManager::shutdown()
 {
 	if (initiated() && !shutdown_) {
 		shutdown_ = true;
-		m_ORB->shutdown();
+		orb_->shutdown();
 	}
 }
--- sources/orbutils/ORBManager.h
+++ sources/orbutils/ORBManager.h
@@ -8,9 +8,9 @@
 //	----------
 //	ORBManager om{argc,argv}; // default name is "ORB"
 //	om.activate_rootpoa();
-//	OO_i obji;
-//	OO_var obj = om.activate_object<OO>(obji);
-//	cout << om.object_to_string(obj.in()) << endl;
+//	T_i obji;
+//	auto obj = om.activate_object<T>(obji);	// auto = T_var
+//	cout << om.object_to_string(obj) << endl;
 //	om.run();
 //
 //	// (with <ENTER> to quit, instead of `om.run()`)
@@ -24,18 +24,18 @@
 //	CLIENT
 //	----------
 //	ORBManager om{argc,argv};
-//	OO_var obj = om.string_to_object<OO>("file://....");
+//	auto obj = om.string_to_object<T>("file://...."); // auto = T_var
 //
 // Notes:
-// ORBManager om;	// non-initialized om: needes init() in order
+// ORBManager om;	// non-initialized om: requires late init() before use
 // om.init(argc, argv);
 //
 // ORB name is optional, but if the same name is provided, the
 // same ORB is used (unless it has been previously destroyed).
 //
 
-#ifndef __ORB_MANAGER_H__
-#define __ORB_MANAGER_H__
+#ifndef ORB_MANAGER_H
+#define ORB_MANAGER_H
 
 #include <string>
 #include <vector>
@@ -44,11 +44,30 @@
 
 namespace colibry {
 
+	enum class POAPolicy : char {
+		SYSTEM_ID, // default
+		USER_ID,
+		IMPLICIT_ACTIVATION, // default
+		NO_IMPLICIT_ACTIVATION,
+		UNIQUE_ID, // default
+		MULTIPLE_ID,
+		TRANSIENT, // default
+		PERSISTENT,
+		USE_ACTIVE_OBJECT_MAP_ONLY,	// default
+		USE_DEFAULT_SERVANT,
+		USE_SERVANT_MANAGER,
+		RETAIN,	// default
+		NON_RETAIN,
+		ORB_CTRL_MODEL,	// default
+		SINGLE_THREAD_MODEL
+	};
+
 	class ORBManager {
+	public:
 	public:
 		ORBManager() = default;						// non-initiated
 		ORBManager(const std::string& orbname);		// initiated w/ default args
-		ORBManager(int argc, char* argv[],
+		ORBManager(int argc, char** argv,
 			const std::string& orbname="ORB");		// will call init()
 		ORBManager(CORBA::ORB_ptr orb);
 		ORBManager(const ORBManager& om);
@@ -60,83 +79,67 @@ namespace colibry {
 		ORBManager& operator=(ORBManager&&) = default;
 
 		[[nodiscard]] std::string name() const { return orbname_; }
-
-		void init(int argc, char* argv[], const std::string& orbname="ORB");
+		void init(int argc, char** argv, const std::string& orbname="ORB");
 		void init(const std::string& orbname="ORB");    // default args
 		[[nodiscard]] bool initiated() const;
+		CORBA::ORB_ptr orb() { return orb_.in(); }
+		void run() { orb_->run(); }
 
 		void activate_rootpoa();
+		PortableServer::POA_ptr rootpoa() { return rootpoa_.in(); }
+		PortableServer::POA_var create_child_poa(const std::string& name,
+			const std::vector<POAPolicy>& policies);
 
-		CORBA::ORB_ptr orb() { return m_ORB.in(); }
-		PortableServer::POA_ptr rootpoa() { return m_rootpoa.in(); }
-
-		enum class Policy : char {
-			SYSTEM_ID, // default
-			USER_ID,
-			IMPLICIT_ACTIVATION, // default
-			NO_IMPLICIT_ACTIVATION,
-			UNIQUE_ID, // default
-			MULTIPLE_ID,
-			TRANSIENT, // default
-			PERSISTENT,
-			USE_ACTIVE_OBJECT_MAP_ONLY,	// default
-    		USE_DEFAULT_SERVANT,
-    		USE_SERVANT_MANAGER,
-    		RETAIN,	// default
-    		NON_RETAIN,
-    		ORB_CTRL_MODEL,	// default
-    		SINGLE_THREAD_MODEL
-		};
-		PortableServer::POA_var create_child_poa(const std::string& name, const std::vector<Policy>& policies);
-
-		template<typename T>
-		T* activate_object(PortableServer::ServantBase& servant);
-
-		void run() { m_ORB->run(); }
-
+		// T_var = TAO_Objref_Var_T<T>
+		template<typename T=CORBA::Object_ptr>
+		TAO_Objref_Var_T<T> activate_object(PortableServer::ServantBase& servant);
 		std::string object_to_string(CORBA::Object_ptr obj);
-		CORBA::Object_ptr string_to_object(const std::string& ior);
+		template<typename T=CORBA::Object_ptr>
+		TAO_Objref_Var_T<T> string_to_object(const std::string& ior);
+		// CORBA::Object_ptr string_to_object(const std::string& ior);
 
-		template<typename T>
-		T* bootstrap(const std::string& service);
-
-		template<typename T>
-		T* string_to_object(const std::string& ior);
+		template<typename T=CORBA::Object_ptr>
+		TAO_Objref_Var_T<T> bootstrap(const std::string& service);
 
 		void save_ior(const std::string& fname, CORBA::Object_ptr obj);
 		void shutdown();
 		[[nodiscard]] bool has_shutdown() const { return shutdown_; }
-
-		[[nodiscard]] bool work_pending() const { return m_ORB->work_pending(); }
-		void perform_work() { m_ORB->perform_work(); }
+		[[nodiscard]] bool work_pending() const { return orb_->work_pending(); }
+		void perform_work() { orb_->perform_work(); }
 	protected:
 		bool shutdown_ = false;
 		void check_init();
 	protected:
 		std::string orbname_;
-		CORBA::ORB_var m_ORB = CORBA::ORB::_nil();
-		PortableServer::POA_var m_rootpoa = PortableServer::POA::_nil();
-		PortableServer::POAManager_var m_poamgr = PortableServer::POAManager::_nil();
+		CORBA::ORB_var orb_ = CORBA::ORB::_nil();
+		PortableServer::POA_var rootpoa_ = PortableServer::POA::_nil();
+		PortableServer::POAManager_var poamgr_ = PortableServer::POAManager::_nil();
 	};
 
+	//
+	// --- template implementations
+	//
+
 	template<typename T>
-	T* ORBManager::bootstrap(const std::string& service) {
-		CORBA::Object_ptr ref = m_ORB->resolve_initial_references(service.c_str());
+	TAO_Objref_Var_T<T> ORBManager::bootstrap(const std::string& service)
+	{
+		CORBA::Object_ptr ref = orb_->resolve_initial_references(service.c_str());
 		return T::_narrow(ref);
 	}
 
 	template<typename T>
-	T* ORBManager::activate_object(PortableServer::ServantBase& servant)
+	TAO_Objref_Var_T<T> /* T* */ ORBManager::activate_object(PortableServer::ServantBase& servant)
 	{
-		PortableServer::ObjectId_var oid = m_rootpoa->activate_object(&servant);
-		CORBA::Object_ptr ref = m_rootpoa->id_to_reference(oid.in());
+		PortableServer::ObjectId_var oid = rootpoa_->activate_object(&servant);
+		CORBA::Object_ptr ref = rootpoa_->id_to_reference(oid.in());
 		return T::_narrow(ref);
 	}
 
 	template<typename T>
-	T* ORBManager::string_to_object(const std::string& ior)
+	TAO_Objref_Var_T<T> /* T* */ ORBManager::string_to_object(const std::string& ior)
 	{
-		CORBA::Object_ptr ref = m_ORB->string_to_object(ior.c_str());
+		check_init();
+		auto ref = orb_->string_to_object(ior.c_str());
 		return T::_narrow(ref);
 	}
 
