2415cf9a341071060c119a3ecd2e4c7b3c6656e9
Luiz Lima Jr <llimajr@gmail.com>
2023-04-22 18:13:26 -0300

    Sort out NameServer.h

--- sources/orbutils/NameServer.h
+++ sources/orbutils/NameServer.h
@@ -6,7 +6,7 @@
 // NameServer needs an existing (EXTERNAL) ORB to run upon.
 // Global NS are possible, but they require late ORB initialization.
 //
-// The name can be in the form of "/c1/c2/name" (dash-separated).
+// The name can be in the form of "/c1/c2/name" (slash-separated).
 //
 
 #ifndef NAMESERVER_H
@@ -21,31 +21,18 @@
 namespace colibry {
 
 	class NameServer {
-	protected:
-
-		CORBA::ORB_var orb_ = CORBA::ORB::_nil();
-		CosNaming::NamingContext_var ns_ = CosNaming::NamingContext::_nil();
-		std::vector<std::string> tounbind_;	// names to automatically unbind
-
-	protected:
-
-		void verify_orb();
-		void init_ns_ref();
-		void verify_nctxs(const CosNaming::Name& n);
-		CORBA::Object_ptr raw_resolve(const std::string& name);
-
 	public:
 
 		NameServer() = default;				// delayed orb initialization
-		NameServer(ORBManager& om);			// om must initiated
-		NameServer(CORBA::ORB_ptr orb);		// orb must initiated
+		NameServer(ORBManager& om);			// om must have been initiated
+		NameServer(CORBA::ORB_ptr orb);		// orb must have been initiated
 		NameServer(const NameServer&) = delete;
 		NameServer(NameServer&&) = default;
 		NameServer& operator=(const NameServer&) = delete;
 		NameServer& operator=(NameServer&&) = delete;
 		virtual ~NameServer() noexcept;
 
-		void use_orb(ORBManager& om);		// om needs to be initiated
+		void use_orb(ORBManager& om);		// om must have been initiated
 		void use_orb(CORBA::ORB_ptr orb);	// orb must be initiated
 
 		template<typename T>
@@ -54,11 +41,24 @@ namespace colibry {
 		}
 
 		// Bind
-		virtual void bind(const std::string& name, CORBA::Object_ptr ref, bool auto_unbind=false);
-		virtual void rebind(const std::string& name, CORBA::Object_ptr ref, bool auto_unbind=false);
+		virtual void bind(const std::string& name, CORBA::Object_ptr ref,
+			bool auto_unbind=false);
+		virtual void rebind(const std::string& name, CORBA::Object_ptr ref,
+			bool auto_unbind=false);
 
 		// Unbind
 		virtual void unbind(const std::string& name);
+
+	protected:
+
+		CORBA::ORB_var orb_ = CORBA::ORB::_nil();
+		CosNaming::NamingContext_var ns_ = CosNaming::NamingContext::_nil();
+		std::vector<std::string> tounbind_;	// names to automatically unbind
+
+		void verify_orb();
+		void init_ns_ref();
+		void verify_nctxs(const CosNaming::Name& n);
+		CORBA::Object_ptr raw_resolve(const std::string& name);
 	};
 
 };
